c68575eaaa9e2b63c820c09aac26eb217d8982f7
tiago1960 <tiagomorais1960@gmail.com>
2020-12-04 16:51:28 -0300

    Criação da branch produtos e models.produto

--- marketdb/models.py
+++ marketdb/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
+from djmoney.models.fields import MoneyField
 
-# Create your models here.
+class Produto(models.Model):
+    autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    dataCriacao = models.DateTimeField(default=timezone.now)
+    titulo = models.CharField(max_length=200)
+    descricao = models.CharField(max_length=400)
+    preco = MoneyField(
+        decimal_places=2,
+        default=0,
+        default_currency='BRL',
+        max_digits=6,
+    )
+    imagem = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100, default='SEM IMAGEM')
+
+
+    def cadastrar(self):
+        self.dataCriacao = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.titulo
--- marketdb/views.py
+++ marketdb/views.py
@@ -1,3 +1,3 @@
 from django.shortcuts import render
+import models
 
-# Create your views here.

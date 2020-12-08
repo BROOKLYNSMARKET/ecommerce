4bfa44ac36ec58027d8fa041a3053a1a87426d45
JuniorMaia2394 <maiajr2393@gmail.com>
2020-12-08 16:18:06 -0300

    Vendo erro de login

--- market/settings.py
+++ market/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'market.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'template/registration')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

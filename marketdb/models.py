c5ff952df170d179ecbc30e7d81fcca147db6c86
helioVictor <heliohelio1@outlook.com>
2020-12-08 15:50:33 -0300

    Criando model Categoria

--- marketdb/models.py
+++ marketdb/models.py
@@ -6,8 +6,8 @@ from djmoney.models.fields import MoneyField
 class Produto(models.Model):
     autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     dataCriacao = models.DateTimeField(default=timezone.now)
-    titulo = models.CharField(max_length=200)
-    descricao = models.CharField(max_length=400)
+    titulo = models.CharField(max_length=50)
+    descricao = models.CharField(max_length=200)
     preco = MoneyField(
         decimal_places=2,
         default=0,
@@ -23,3 +23,14 @@ class Produto(models.Model):
 
     def __str__(self):
         return self.titulo
+
+class categoriaProduto(models.Model):
+    nome = models.CharField(max_length=50)
+    dataCriacao = models.DateTimeField(default=timezone.now)
+
+    def cadastrar(self):
+        self.dataCriacao = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.nome
